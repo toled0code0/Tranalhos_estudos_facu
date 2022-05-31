438a748be6570c94855798e1e403d401cfc20a8b
toled0code0 <106595959+toled0code0@users.noreply.github.com>
2022-05-31 18:09:49 -0300

    Conversor de moeda brasil pra dolar.
    Trabalho pra faculdade

---
+++ conversor_rs_dl.py
@@ -0,0 +1,14 @@
+#Programas para converção de moedas em python
+print( '--------------------------------------------' )
+print( 'Bem-vindo ao convertor spain.py de Dolar pra Real' )
+print( '--------------------------------------------' )
+#Algoritimo 
+mo_1 = float(input("Digite o valor do Dolar atual: ")) 
+mo_3 = float(input("Digite o valor do Real Brasileiro: "))
+mo_2 = float(input("Digite o valor da converçãol: "))
+conv = (mo_1*mo_2)
+#Print's
+print('---------------------------------------------')
+print("{} R$ :Dolar Americano atual.".format(mo_1))
+print("                                            Valor da Converção:{} R$ ".format(conv))
+print("{} $ :Real Brasileiro atual".format(mo_3))
