f0a185984e493ed06713659836c56261f9e53ca6
toled0code0 <106595959+toled0code0@users.noreply.github.com>
2022-05-31 16:49:32 -0300

    Create tabauada_div.py

---
+++ tabauada_div.py
@@ -0,0 +1,6 @@
+  #tabuada de diviir com input:
+num = float(input('Digite o numero que voçê deseja fazer a divisão: '))
+num_2 = float(input('Digite o numero desejavel: '))
+  #Codigo da operação: 
+resultado = (num/num_2)
+print(('O resultado da operação é:'), resultado )
